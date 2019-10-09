07ea82877d84bf71aba324f5b98f9aa3ee680d2b
mascaradew <williamyamada16@gmail.com>
2019-10-09 11:31:46 -0300

    struct

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
     no console (terminal de comandos)
 
     para compilar: gcc main.c tetris.c display.c -o programa
+                   ou  .\compile
     Para executar: start .\programa
 
     Autor: William Yamada
@@ -12,6 +13,7 @@
 
 #include "tetris.h"
 #include "display.h"
+#define DEBUG 1
 
 /*
     Parte principal do programa, responsável por iniciar e 
@@ -19,12 +21,16 @@
 */
 int main(){
     char matrix[ROWS][COLUMNS];
-    int posI, posJ;
+    Bloco tijolo;
     int keypressed=0;
 
-    //posicao inicial do personagem
-    posI = 0;
-    posJ = COLUMNS/2;
+    //posicao inicial do personagem e sua forma
+    tijolo.i = 0;
+    tijolo.j = COLUMNS/2;
+    tijolo.tipo = TIPO_I;
+    tijolo.orientacao = ORIENTACAO_UP;
+    tijolo.width = 1;
+    tijolo.height = 4;
     //inicializando matriz
     init(matrix);
 
@@ -36,17 +42,45 @@ int main(){
     while(keypressed != ESC){        
         gotoxy(0,0);
 
-        //posicionar o @ no meio da tela
-        matrix[posI][posJ] = '@';
+        #if DEBUG == 1
+            printf("PIXEL = (%d,%d)\n\n", tijolo.i, tijolo.j);
+        #endif
 
+        //posicionar o @ no meio da tela
+        switch (tijolo.orientacao){
+            case ORIENTACAO_UP:
+                if (tijolo.i-3>=0) matrix[tijolo.i-3][tijolo.j] = PIXEL;
+                if (tijolo.i-2>=0) matrix[tijolo.i-2][tijolo.j] = PIXEL;
+                if (tijolo.i-1>=0) matrix[tijolo.i-1][tijolo.j] = PIXEL;
+                matrix[tijolo.i][tijolo.j] = PIXEL;
+            break;
+            case ORIENTACAO_LEFT:
+                if (tijolo.i-3>=0) matrix[tijolo.i][tijolo.j-3] = PIXEL;
+                if (tijolo.i-2>=0) matrix[tijolo.i][tijolo.j-2] = PIXEL;
+                if (tijolo.i-1>=0) matrix[tijolo.i][tijolo.j-1] = PIXEL;
+                matrix[tijolo.i][tijolo.j] = PIXEL;
+            break;
+        }
         //mostro a matriz na tela
         printMatrix(matrix);
 
         //faça posição anterior do @ ser apagada
-        matrix[posI][posJ] = ' ';
-        
+        switch (tijolo.orientacao){
+            case ORIENTACAO_UP:
+                if (tijolo.i-3>=0) matrix[tijolo.i-3][tijolo.j] = EMPY;
+                if (tijolo.i-2>=0) matrix[tijolo.i-2][tijolo.j] = EMPY;
+                if (tijolo.i-1>=0) matrix[tijolo.i-1][tijolo.j] = EMPY;
+                matrix[tijolo.i][tijolo.j] = EMPY;
+            break;
+            case ORIENTACAO_LEFT:
+                if (tijolo.i-3>=0) matrix[tijolo.i][tijolo.j-3] = EMPY;
+                if (tijolo.i-2>=0) matrix[tijolo.i][tijolo.j-2] = EMPY;
+                if (tijolo.i-1>=0) matrix[tijolo.i][tijolo.j-1] = EMPY;
+                matrix[tijolo.i][tijolo.j] = EMPY;
+            break;
+        }
         //faço a posição da @ ir para a baixo
-        if(posI < (ROWS-1)) posI++;
+        if(tijolo.i < (ROWS-1)) tijolo.i++;
 
         //lendo teclas
         keypressed = 0;
@@ -56,13 +90,15 @@ int main(){
         switch(keypressed){
             //se move para esquerda
             case LEFT: 
+            case TECLA_a:
             case TECLA_A:
-                if(posJ > 0) posJ--;
+                if(tijolo.j > 0) tijolo.j--;
             break;
             //se move para direita
             case RIGHT:
-            case TECLA_D: 
-                if(posJ < (COLUMNS - 1)) posJ++;
+            case TECLA_d: 
+            case TECLA_D:
+                if(tijolo.j < (COLUMNS - 1)) tijolo.j++;
             break;
         }
 
--- tetris.h
+++ tetris.h
@@ -2,7 +2,7 @@
     Jogo interativo Tetris implementado em linguagem C para uso 
     no console (terminal de comandos)
 
-    Autor: Augusto Luengo Pereira Nunes
+    Autor: William Yamada
     Data: 28/08/2019
 
 */
@@ -19,8 +19,37 @@
 #define ARROWS 224
 #define LEFT 75
 #define RIGHT 77
-#define TECLA_A 97
-#define TECLA_D 100
+#define TECLA_a 97
+#define TECLA_d 100
+#define TECLA_A 65
+#define TECLA_D 68
+//Criando as orientações de peças
+#define ORIENTACAO_UP 1
+#define ORIENTACAO_DOWN 2
+#define ORIENTACAO_LEFT 3
+#define ORIENTACAO_RIGHT 4
+//Os tipos de peças
+#define TIPO_L 1
+#define TIPO_L_R 2 //L reverso
+#define TIPO_T 3
+#define TIPO_Z 4
+#define TIPO_Z_R 5 //Z reverso
+#define TIPO_O 6 // Quadrado
+#define TIPO_I 7
+
+//configurações de layout
+#define PIXEL 219
+#define EMPY 32
+
+//Estrutura padrão de componentes
+typedef struct{
+    int i; //posicao na linha
+    int j; //posicao na coluna
+    int orientacao;
+    int tipo; //tipo da peça entre os 7
+    int width; //largura da peça
+    int height; //altura da peça
+}Bloco;
 
 /*
     Inicializa a matriz principal com 'espaços vazios'
@@ -32,3 +61,4 @@ void init(char matrix[ROWS][COLUMNS]);
     do computador.
 */
 void printMatrix(char matrix[ROWS][COLUMNS]);
+
